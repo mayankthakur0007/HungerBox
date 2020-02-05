129bdfbaf0497851a0dcd79aee397af269d26d57
mayankthakur0007 <mayankthakur231@gmail.com>
2020-02-05 11:58:29 +0530

    changes

--- src/HungerBox-app/HungerBox-app.js
+++ src/HungerBox-app/HungerBox-app.js
@@ -22,142 +22,139 @@ setPassiveTouchGestures(true);
 setRootPath(MyAppGlobals.rootPath);
 
 class HungerBoxApp extends PolymerElement {
-  static get template() {
-    return html`
-      <style>
-        :host {
-          --app-primary-color: #4285f4;
-          --app-secondary-color: black;
+static get template() {
+return html`
+<style>
+  :host {
+    --app-primary-color: #ff7a22;
+    --app-secondary-color: black;
 
-          display: block;
-        }
-
-        app-drawer-layout:not([narrow]) [drawer-toggle] {
-          display: none;
-        }
-
-        app-header {
-          color: #fff;
-          background-color: var(--app-primary-color);
-        }
-
-        app-header paper-icon-button {
-          --paper-icon-button-ink-color: white;
-        }
-
-        .drawer-list {
-          margin: 0 20px;
-        }
-
-        .drawer-list a {
-          display: block;
-          padding: 0 16px;
-          text-decoration: none;
-          color: var(--app-secondary-color);
-          line-height: 40px;
-        }
-
-        .drawer-list a.iron-selected {
-          color: black;
-          font-weight: bold;
-        }
-      </style>
-
-      <app-location route="{{route}}">
-      </app-location>
-
-      <app-route route="{{route}}" pattern="[[rootPath]]:page" data="{{routeData}}" tail="{{subroute}}">
-      </app-route>
-
-      <app-drawer-layout fullbleed="" narrow="{{narrow}}">
-        <!-- Drawer content -->
-        <app-drawer id="drawer" slot="drawer" swipe-open="[[narrow]]">
-          <app-toolbar>Menu</app-toolbar>
-          <iron-selector selected="[[page]]" attr-for-selected="name" class="drawer-list" role="navigation">
-            <a name="view1" href="[[rootPath]]view1">View One</a>
-            <a name="view2" href="[[rootPath]]view2">View Two</a>
-            <a name="view3" href="[[rootPath]]view3">View Three</a>
-          </iron-selector>
-        </app-drawer>
-
-        <!-- Main content -->
-        <app-header-layout has-scrolling-region="">
-
-          <app-header slot="header" condenses="" reveals="" effects="waterfall">
-            <app-toolbar>
-              <paper-icon-button icon="my-icons:menu" drawer-toggle=""></paper-icon-button>
-              <div main-title="">My App</div>
-            </app-toolbar>
-          </app-header>
-
-          <iron-pages selected="[[page]]" attr-for-selected="name" role="main">
-            <my-view1 name="view1"></my-view1>
-            <my-view2 name="view2"></my-view2>
-            <my-view3 name="view3"></my-view3>
-            <my-view404 name="view404"></my-view404>
-          </iron-pages>
-        </app-header-layout>
-      </app-drawer-layout>
-    `;
+    display: block;
   }
 
-  static get properties() {
-    return {
-      page: {
-        type: String,
-        reflectToAttribute: true,
-        observer: '_pageChanged'
-      },
-      routeData: Object,
-      subroute: Object
-    };
+  app-drawer-layout:not([narrow]) [drawer-toggle] {
+    display: none;
   }
 
-  static get observers() {
-    return [
-      '_routePageChanged(routeData.page)'
-    ];
+  app-header {
+    color: #fff;
+    background-color: var(--app-primary-color);
   }
 
-  _routePageChanged(page) {
-     // Show the corresponding page according to the route.
-     //
-     // If no page was found in the route data, page will be an empty string.
-     // Show 'view1' in that case. And if the page doesn't exist, show 'view404'.
-    if (!page) {
-      this.page = 'view1';
-    } else if (['view1', 'view2', 'view3'].indexOf(page) !== -1) {
-      this.page = page;
-    } else {
-      this.page = 'view404';
-    }
-
-    // Close a non-persistent drawer when the page & route are changed.
-    if (!this.$.drawer.persistent) {
-      this.$.drawer.close();
-    }
+  app-header paper-icon-button {
+    --paper-icon-button-ink-color: white;
   }
 
-  _pageChanged(page) {
-    // Import the page component on demand.
-    //
-    // Note: `polymer build` doesn't like string concatenation in the import
-    // statement, so break it up.
-    switch (page) {
-      case 'view1':
-        import('./my-view1.js');
-        break;
-      case 'view2':
-        import('./my-view2.js');
-        break;
-      case 'view3':
-        import('./my-view3.js');
-        break;
-      case 'view404':
-        import('./my-view404.js');
-        break;
-    }
+  h3 {
+    font-family: Comic Sans, Comic Sans MS, cursive;
   }
+
+  .drawer-list {
+    margin: 0 20px;
+  }
+
+  .drawer-list a {
+    display: block;
+    padding: 0 16px;
+    text-decoration: none;
+    color: var(--app-secondary-color);
+    line-height: 40px;
+  }
+
+  .drawer-list a.iron-selected {
+    color: black;
+    font-weight: bold;
+  }
+</style>
+
+<app-location route="{{route}}">
+</app-location>
+
+<app-route route="{{route}}" pattern="[[rootPath]]:page" data="{{routeData}}" tail="{{subroute}}">
+</app-route>
+
+<app-drawer-layout fullbleed="" narrow="{{narrow}}">
+  <!-- Drawer content -->
+  <app-drawer id="drawer" slot="drawer" swipe-open="[[narrow]]">
+    <app-toolbar>Menu</app-toolbar>
+    <iron-selector selected="[[page]]" attr-for-selected="name" class="drawer-list" role="navigation">
+      <a name="login" href="[[rootPath]]login">Login</a>
+      <a name="view2" href="[[rootPath]]view2">My Profie</a>
+      <a name="view5" href="[[rootPath]]view5">My Orders</a>
+      <a name="view3" href="[[rootPath]]view3">About Us</a>
+      <a name="login" href="[[rootPath]]login">Logout</a>
+    </iron-selector>
+  </app-drawer>
+
+  <!-- Main content -->
+  <app-header-layout has-scrolling-region="">
+
+    <app-header slot="header" condenses="" reveals="" effects="waterfall">
+      <app-toolbar>
+        <paper-icon-button icon="my-icons:menu" drawer-toggle=""></paper-icon-button>
+        <div main-title="">
+          <h3>Yummy Tummy</h3>
+        </div>
+      </app-toolbar>
+    </app-header>
+
+    <iron-pages selected="[[page]]" attr-for-selected="name" role="main">
+      <login-form name="login"></login-form>
+      <my-view1 name="view1"></my-view1>
+      <my-view404 name="view404"></my-view404>
+    </iron-pages>
+  </app-header-layout>
+</app-drawer-layout>
+`;
+}
+
+static get properties() {
+return {
+page: {
+type: String,
+reflectToAttribute: true,
+observer: '_pageChanged'
+},
+routeData: Object,
+subroute: Object
+};
+}
+
+static get observers() {
+return [
+'_routePageChanged(routeData.page)'
+];
+}
+
+_routePageChanged(page) {
+// Show the corresponding page according to the route.
+//
+// If no page was found in the route data, page will be an empty string.
+// Show 'view1' in that case. And if the page doesn't exist, show 'view404'.
+this.page = page || 'login';
+
+}
+
+_pageChanged(page) {
+// Import the page component on demand.
+//
+// Note: `polymer build` doesn't like string concatenation in the import
+// statement, so break it up.
+switch (page) {
+case 'login':
+import('./login-form.js');
+break;
+case 'view1':
+import('./my-view1.js');
+break;
+case 'view3':
+import('./my-view3.js');
+break;
+case 'view404':
+import('./my-view404.js');
+break;
+}
+}
 }
 
 window.customElements.define('hunger-app', HungerBoxApp);
---
+++ src/HungerBox-app/login-form.js
@@ -0,0 +1,157 @@
+import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
+import '../../node_modules/@polymer/paper-button/paper-button.js';
+import '../../node_modules/@polymer/paper-dropdown-menu/paper-dropdown-menu.js';
+import '../../node_modules/@polymer/paper-listbox/paper-listbox.js';
+import '../../node_modules/@polymer/paper-item/paper-item.js';
+import '../../node_modules/@polymer/paper-input/paper-input.js';
+import '../../node_modules/@polymer/app-route/app-location.js';
+
+/**
+ * @customElement
+ * @polymer
+ */
+
+class LoginForm extends PolymerElement {
+    static get template() {
+        return html`
+        <style>
+        :host {
+          display: block;
+        }
+        #btn{
+          margin:10px 10px 10px 160px;
+         background-color:  #ff7a22;
+         color: white;
+        }
+        
+        #input{
+          display:flex;
+          flex-direction:column;
+          flex-wrap:wrap;
+        }
+        .items{
+          margin:10px;
+         
+        }
+
+        #start{
+          margin-top:-18px;
+        }
+       form{
+          padding:2px;
+        }
+        #loginLayout{
+            border-radius:20px;
+            background-color: white;
+            width:450px;
+            padding: 20px;
+            margin:100px 250px 150px 250px;
+        }
+      </style>
+      <app-location route="{{route}}">
+      </app-location>
+      <iron-form id="loginForm">
+      <form><div id="loginLayout">
+      <div id="input">
+        <div class="items"> <paper-input type="email" label="Email ID" id="email"></paper-input></div>
+        <div class="items"> <paper-input type="password" label="Password" id="password"></paper-input></div>
+      </div>
+        <div class="items"><paper-button type="submit" id="btn" class="btn btn-success" on-click="handleLogin">Login</paper-button></div>
+        </div>
+      </form>
+      </iron-form>
+
+      <iron-ajax id="ajax" on-response="_handleResponse" handle-as="json" content-type='application/json'></iron-ajax>
+    `;
+    }
+
+    static get properties() {
+        return {
+            page: {
+                type: String,
+                reflectToAttribute: true,
+                observer: '_pageChanged'
+            },
+            selected: {
+                type: Number,
+                value: 0
+            },
+            routeData: Object,
+            subroute: Object,
+            action: {
+                type: String,
+                value: 'List'
+            },
+            users: {
+                type: Array,
+                value: []
+            },
+            valid: {
+                type: String,
+                value: "no"
+            }
+        };
+    }
+    connectedCallback() {
+        super.connectedCallback();
+        this._getData();
+    }
+    /** getdata function for fetching the data from the database and showing it. Ajax request is done in pets
+    
+    for the data with sell property value "yes" only. This function is also called when any new pet is added 
+    
+    so that the list got again refreshed **/
+
+    _getData() {
+        this._makeAjax(`http://localhost:3000/users`, "get", null);
+    }
+    handleLogin(event) {
+        event.preventDefault();
+        let email = this.$.email.value;
+        let password = this.$.password.value;
+        let users = this.users;
+        if (email == '' || password == '') {
+            alert("enter details");
+            return false;
+        } else {
+            for (let i = 0; i < users.length; i++) {
+                if (users[i].email == email && users[i].password == password) {
+                    this.valid = "yes";
+                  
+                    let userdata =  users[i];
+                    let phone = users[i].mobile;
+                    sessionStorage.setItem('phone', phone);
+                    userdata = JSON.stringify(userdata);
+                    sessionStorage.setItem('userdata', userdata);
+                    sessionStorage.setItem('email', email);
+                    sessionStorage.setItem('login', 'true');
+                    this.$.loginForm.reset();
+                    this.dispatchEvent(new CustomEvent('refresh-login', { detail: { item: true, email: email }, bubbles: true, composed: true }));
+                    this.set('route.path', 'home');
+                }
+            } if (this.valid == "no") {
+                alert("wrong");
+            }
+        }
+    }
+    _makeAjax(url, method, postObj) {
+        const ajax = this.$.ajax;
+        ajax.method = method;
+        ajax.url = url;
+        ajax.body = postObj ? JSON.stringify(postObj) : undefined;
+        ajax.generateRequest();
+    }
+    _handleResponse(event) {
+
+        switch (this.action) {
+
+            case 'List':
+                this.users = event.detail.response;
+                break;
+        }
+
+
+    }
+
+}
+window.customElements.define('login-form', LoginForm);
