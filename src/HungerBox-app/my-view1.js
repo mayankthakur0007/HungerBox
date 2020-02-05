2dd5df5812884db93faa59d173816e779b53e831
amitkumar392 <60509339+amitkumar392@users.noreply.github.com>
2020-02-05 11:52:28 +0530

    commit 5

--- src/HungerBox-app/my-view1.js
+++ src/HungerBox-app/my-view1.js
@@ -10,6 +10,11 @@
 
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 import './shared-styles.js';
+import '@polymer/paper-dropdown-menu/paper-dropdown-menu.js';
+import '@polymer/paper-listbox/paper-listbox.js';
+import '@polymer/paper-item/paper-item.js';
+import '@polymer/paper-card/paper-card.js';
+import '@polymer/paper-button/paper-button.js';
 
 class MyView1 extends PolymerElement {
   static get template() {
@@ -17,17 +22,74 @@ class MyView1 extends PolymerElement {
       <style include="shared-styles">
         :host {
           display: block;
-
-          padding: 10px;
+         padding: 10px;
+        }
+        paper-card{
+          width:500px;
+          margin:10px;
+          border: 2px solid black;
+          border-radius:2px;
+        }
+        paper-button{
+          background-color:orange; 
         }
       </style>
 
-      <div class="card">
-        <div class="circle">1</div>
-        <h1>View One</h1>
-        <p>Ut labores minimum atomorum pro. Laudem tibique ut has.</p>
-        <p>Lorem ipsum dolor sit amet, per in nusquam nominavi periculis, sit elit oportere ea.Lorem ipsum dolor sit amet, per in nusquam nominavi periculis, sit elit oportere ea.Cu mei vide viris gloriatur, at populo eripuit sit.</p>
-      </div>
+      
+
+      <paper-dropdown-menu label="Working Location" id="workingLocation" required>
+      <paper-listbox slot="dropdown-content" selected="">
+       <paper-item>Surya Sapphire</paper-item>
+       <paper-item>Jigni</paper-item>
+       </paper-listbox>
+     </paper-dropdown-menu> <br>
+
+     
+<paper-card heading="" image="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Snack It">
+<div class="card-content">
+<h2>Snack It</h2>
+  </div>
+<div class="card-actions">
+  
+  <paper-button raised>Order Here </paper-button>
+</div>
+</paper-card>
+
+<paper-card heading="" image="https://media.gettyimages.com/photos/authentic-indian-food-picture-id639389404?s=612x612" alt="Give Me 5">
+<div class="card-content">
+<h2>Give Me 5</h2>
+  </div>
+<div class="card-actions">
+<paper-button raised>Order Here </paper-button>
+
+</div>
+</paper-card>
+
+
+<paper-card heading="" image="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Dosa Talks">
+<div class="card-content">
+<h2>Dosa Talks</h2>
+ 
+</div>
+<div class="card-actions">
+<paper-button raised>Order Here </paper-button>
+
+</div>
+</paper-card>
+
+<paper-card heading="" image="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Go Nature">
+<div class="card-content">
+<h2>Go Nature</h2>
+ 
+</div>
+<div class="card-actions">
+<paper-button raised>Order Here </paper-button>
+ 
+</div>
+</paper-card>
+
+
+     
     `;
   }
 }
